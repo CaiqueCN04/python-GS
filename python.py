c5df61bbb87e77e5fe30649b90798aa826d0e857
Rafael <cchagas04@gmail.com>
2023-11-24 15:17:01 -0300

    validações finalizadas

--- python.py
+++ python.py
@@ -1,18 +1,18 @@
 import json
 import requests
-
+ 
 def cadastrar_usuario(tipo_usuario):
+
+
     while True:
+        
     # Função para cadastrar médico ou paciente
         if tipo_usuario == 'medico':
             # Solicitar informações específicas do médico
             crm = input("Digite o CRM: ")
-            usuario = {"crm": crm}
-        elif tipo_usuario == 'paciente':
-            
-            # Solicitar informações específicas do paciente
+             # Solicitar informações específicas do paciente
             cep = input("Digite o CEP: ")
-
+ 
             endereco = buscar_cep(cep)
             print("\nConfirme as informações do CEP:")
             print(f"CEP: {endereco['cep']}")
@@ -21,40 +21,78 @@ def cadastrar_usuario(tipo_usuario):
             print(f"Bairro: {endereco['bairro']}")
             print(f"Localidade: {endereco['localidade']}")
             print(f"UF: {endereco['uf']}")
-            
+           
             confirmar_cep = input("As informações do CEP estão corretas? (s/n): ").lower()
             if confirmar_cep != 's':
                 print("Cadastro cancelado.")
                 return
-
+ 
             if not endereco:
                 print('CEP não encontrado. Verifique o CEP e tente novamente.')
                 return
-
+ 
             rua = endereco.get('logradouro', '')
             bairro = endereco.get('bairro', '')
             municipio = endereco.get('localidade', '')
             uf = endereco.get('uf', '')
             complemento = endereco.get('complemento','')
-
+ 
             numero = input("Digite o número da residência: ")
             complemento = input("Digite o complemento: ")
-            usuario = {"cep": cep, "numero": numero, "complemento": complemento}
+            usuario = {"crm": crm, "cep": cep,"UF":uf,"Município":municipio,"Bairro":bairro,"Rua":rua, "numero": numero, "complemento": complemento}
+        elif tipo_usuario == 'paciente':
+           
+            # Solicitar informações específicas do paciente
+            cep = input("Digite o CEP: ")
+ 
+            endereco = buscar_cep(cep)
+            print("\nConfirme as informações do CEP:")
+            print(f"CEP: {endereco['cep']}")
+            print(f"Logradouro: {endereco['logradouro']}")
+            print(f"Complemento: {endereco['complemento']}")
+            print(f"Bairro: {endereco['bairro']}")
+            print(f"Localidade: {endereco['localidade']}")
+            print(f"UF: {endereco['uf']}")
+           
+            confirmar_cep = input("As informações do CEP estão corretas? (s/n): ").lower()
+            if confirmar_cep != 's':
+                print("Cadastro cancelado.")
+                return
+ 
+            if not endereco:
+                print('CEP não encontrado. Verifique o CEP e tente novamente.')
+                return
+ 
+            rua = endereco.get('logradouro', '')
+            bairro = endereco.get('bairro', '')
+            municipio = endereco.get('localidade', '')
+            uf = endereco.get('uf', '')
+            complemento = endereco.get('complemento','')
+ 
+            numero = input("Digite o número da residência: ")
+            complemento = input("Digite o complemento: ")
+            usuario = {"cep": cep,"UF":uf,"Município":municipio,"Bairro":bairro,"Rua":rua, "numero": numero, "complemento": complemento}
         else:
             print("Tipo de usuário inválido.")
             return
-
+ 
         # Solicitar informações comuns a ambos os usuários
         nome = input("Digite o nome: ")
-
+ 
         cpf = input("Digite o CPF: ")
         if len(cpf) != 11 or not cpf.isdigit():
-            print("O número do CPF de identificação deve conter 11 dígitos numéricos. Tente novamente.")       
+            print("O número do CPF de identificação deve conter 11 dígitos numéricos. Tente novamente.")      
             continue
-        
+       
         email = input("Digite o email: ")
+        if "@" not in email:
+            print("o email deve ter um @")
+            continue
         senha = input("Digite a senha: ")
-
+        if len(senha) != 8:
+            print("a senha deve conter no minimo 8 caracteres")
+            continue
+ 
         # Adicionar informações ao arquivo JSON correspondente
         arquivo_json = f"{tipo_usuario}s.json"
         try:
@@ -63,12 +101,8 @@ def cadastrar_usuario(tipo_usuario):
         except (json.decoder.JSONDecodeError, FileNotFoundError):
             # Se o arquivo estiver vazio ou com formato inválido, cria uma lista vazia
             usuarios = []
-
-        with open(arquivo_json, 'w') as arquivo:
-            # Adiciona o novo usuário à lista existente
-            usuarios.append({"cpf": cpf, "nome": nome, "email": email, "senha": senha, **usuario})
-            json.dump(usuarios, arquivo, indent=2)
-        return
+ 
+        
 def buscar_cep(cep):
     while True:
         try:
@@ -95,18 +129,18 @@ def buscar_cep(cep):
         except requests.exceptions.ConnectTimeout:
             print('Erro ao carregar API, aguarde..')
             continue
-
+ 
 def realizar_login(tipo_usuario):
     # Função para realizar login como médico ou paciente
-    cpf_ou_crm = input("Digite o CPF ou CRM: ")
+    cpf = input("Digite o CPF: ")
     senha = input("Digite a senha: ")
-
+ 
     # Verificar se o usuário existe no arquivo JSON correspondente
     arquivo_json = f"{tipo_usuario}s.json"
     try:
         with open(arquivo_json, 'r') as arquivo:
             usuarios = json.load(arquivo)
-            usuario = next((u for u in usuarios if u.get("cpf") == cpf_ou_crm), None)
+            usuario = next((u for u in usuarios if u.get("cpf") == cpf), None)
             if usuario and usuario["senha"] == senha:
                 print("Login bem-sucedido.")
                 if tipo_usuario == 'medico':
@@ -117,64 +151,94 @@ def realizar_login(tipo_usuario):
                 print("Credenciais inválidas.")
     except (json.decoder.JSONDecodeError, FileNotFoundError):
         print("Nenhum usuário cadastrado. Faça o cadastro primeiro.")
-
+ 
+def medico_presencial():
+    print('1 - Analisar diagnósticos')
+    print('0 - Sair do programa')
+    opcao = input('Escolha:')
+    if opcao == '1':
+        print('\nAguardando pré-diagnósticos para análise\n')
+        medico_presencial()
+    elif opcao == '0':
+        return
+   
+def medico_online():
+    print("1 - Validar pré-diagnósticos")
+    print("2 - Ficar disponível para telemedicina")
+    print("0 - Sair do programa")
+    opcao = input("Escolha uma opção: ")
+    if opcao == '1':
+        print('Nenhum pré-diagnóstico para validação disponível até o momento')
+        medico_online()
+    elif opcao == '2':
+        print("\nAguardando por paciente...\n")
+        medico_online()
+    elif opcao == '0':
+        return
+ 
 def menu_medico():
     # Menu para médicos
-    print("1. Presencial em um hospital")
-    print("2. Online")
+    print('\nVocê está trabalhando:\n')
+    print("1 - Presencial em um hospital")
+    print("2 - Online")
     opcao = input("Escolha uma opção: ")
-
+ 
     if opcao == '1':
-        print("Opções para analisar pré-diagnósticos ou sair do programa.")
+        medico_presencial()
     elif opcao == '2':
-        print("1. Validar pré-diagnósticos")
-        print("2. Ficar disponível para telemedicina")
-        print("3. Sair do programa")
-        opcao = input("Escolha uma opção: ")
-        if opcao == '2':
-            print("Aguardando por paciente... (Pressione 0 para retornar ao menu)")
-
+        medico_online()
+    else:
+        print('Opção invalida, tente novamente')
+           
+ 
 def menu_paciente():
-   
+  while True:
     # Menu para pacientes
-    print("1. Pré-diagnóstico com chatbot")
-    print("2. Marcar exames")
-    print("3. Sair do programa")
+    print("1 - Pré-diagnóstico com chatbot")
+    print("2 - Marcar exames")
+    print("0 - Sair do programa")
     opcao = input("Escolha uma opção: ")
-
+ 
     if opcao == '2':
-        print("Ainda em desenvolvimento. (Pressione 0 para voltar ao menu)")
-
+        print("\nAinda em desenvolvimento...\n")
+ 
     elif opcao == '1':
         print("Quais sintomas você está sentindo?")
         sintomas = input()
         print("Há quanto tempo você está sentindo cada sintoma?")
         tempo_sintomas = input()
-        print("Diagnóstico por IA ainda em desenvolvimento.")
-        print("1. Atendimento por telemedicina")
-        print("2. Atendimento em hospital")
-        print("0. Voltar")
+        print("\nDiagnóstico por IA ainda em desenvolvimento.\n")
+        print("1 - Atendimento por telemedicina")
+        print("2 - Atendimento em hospital")
+        print("0 - Voltar")
         opcao = input("Escolha uma opção: ")
         if opcao == '1':
-            print("Aguardando atendimento por telemedicina...")
+            print("\nAguardando atendimento por telemedicina...\n")
         elif opcao == '2':
-            print("Dirija-se ao hospital mais próximo.")
+            print("\nDirija-se ao hospital mais próximo.\n")
         elif opcao == '0':
             menu_paciente()
-
+    elif opcao == '0':
+        return
+ 
 # Programa principal
 print("Bem-vindo ao sistema médico!")
-tipo_usuario = input("Você é um médico ou paciente? Digite 'medico' ou 'paciente': ")
-
+print('Você é um:')
+print(' - Médico')
+print(' - Paciente')
+tipo_usuario = input("Digite 'medico' ou 'paciente': ")
+ 
 while True:
-    opcao = input("Escolha 1 para login, 2 para cadastro ou 0 para sair: ")
-
+    opcao = input("Escolha:\n1 - para login\n2 - para cadastro\n3 - para mudar o tipo de usuário \n0 - para encerrar programa\n")
+ 
     if opcao == '1':
         realizar_login(tipo_usuario)
     elif opcao == '2':
         cadastrar_usuario(tipo_usuario)
+    elif opcao == '3':
+        tipo_usuario = input('Deseja entrar como medico ou paciente:\n')
     elif opcao == '0':
-        print("Saindo do programa. Até logo!")
+        print("Encerrando programa. Até logo!")
         break
     else:
         print("Opção inválida. Tente novamente.")
